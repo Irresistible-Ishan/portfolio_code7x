def6bfbdb1f4930959d1eae0fdce9027adfdacbc
Irresistible-Ishan <1ishanmanisingh@gmail.com>
2024-07-12 21:10:13 +0530

    update

--- script.js
+++ script.js
@@ -61,3 +61,18 @@ function generateRandomColor() {
 window.onload = function() {
    createFallingElements();
 };
+
+
+
+function updateClock() {
+    var now = new Date();
+    var hours = now.getHours().toString().padStart(2, '0');
+    var minutes = now.getMinutes().toString().padStart(2, '0');
+    var seconds = now.getSeconds().toString().padStart(2,'0')
+    var time = hours + ':' + minutes + ':' + seconds;
+
+    document.getElementById('clockText').textContent = time;
+}
+
+// Update clock every second
+setInterval(updateClock, 1000);
